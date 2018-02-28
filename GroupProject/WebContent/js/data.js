8ed7e060edeb038eb45a05827bab4edccd3fa225
lichenke <llw04758@gmail.com>
2018-02-28 16:27:55 +0000

    add the Gantt chart js files

---
+++ GroupProject/WebContent/js/data.js
@@ -0,0 +1,57 @@
+var ganttData = [
+	{
+		id: 1, name: "document specification", series: [
+			{ name: "Planned", start: new Date(2018,00,01), end: new Date(2018,0,5) },
+			{ name: "Actual", start: new Date(2018,00,02), end: new Date(2018,00,05), color: "#f0f0f0" }
+		]
+	}, 
+	{
+		id: 2, name: "test ", series: [
+			{ name: "Planned", start: new Date(2018,00,05), end: new Date(2018,00,20) },
+			{ name: "Actual", start: new Date(2018,00,06), end: new Date(2018,00,17), color: "#f0f0f0" },
+			{ name: "Projected", start: new Date(2018,00,06), end: new Date(2018,00,17), color: "#4e625e" }
+		]
+	}, 
+	{
+		id: 3, name: "test1", series: [
+			{ name: "Planned", start: new Date(2018,00,11), end: new Date(2018,01,03) },
+			{ name: "Actual", start: new Date(2018,00,15), end: new Date(2018,01,03), color: "#f0f0f0" }
+		]
+	}, 
+	{
+		id: 4, name: "Front end", series: [
+			{ name: "Planned", start: new Date(2018,01,01), end: new Date(2018,01,03) },
+			{ name: "Actual", start: new Date(2018,01,01), end: new Date(2018,01,05), color: "#f0f0f0" }
+		]
+	},
+	{
+		id: 5, name: "back end", series: [
+			{ name: "Planned", start: new Date(2018,02,01), end: new Date(2018,03,20) },
+			{ name: "Actual", start: new Date(2018,02,01), end: new Date(2018,03,26), color: "#f0f0f0" }
+		]
+	}, 
+	{
+		id: 6, name: "database", series: [
+			{ name: "Planned", start: new Date(2018,00,05), end: new Date(2018,00,20) },
+			{ name: "Actual", start: new Date(2018,00,06), end: new Date(2018,00,17), color: "#f0f0f0" },
+			{ name: "Projected", start: new Date(2018,00,06), end: new Date(2018,00,20), color: "#4e625e" }
+		]
+	}, 
+	{
+		id: 7, name: "blabla", series: [
+			{ name: "Planned", start: new Date(2018,00,11), end: new Date(2018,01,03) }
+		]
+	}, 
+	{
+		id: 8, name: "Feature 8", series: [
+			{ name: "Planned", start: new Date(2018,01,01), end: new Date(2018,01,03) },
+			{ name: "Actual", start: new Date(2018,01,01), end: new Date(2018,01,05), color: "#f0f0f0" }
+		]
+	},
+		{
+		id: 9, name: "Feature 9", series: [
+			{ name: "Planned", start: new Date(2018,01,01), end: new Date(2018,01,03) },
+			{ name: "Actual", start: new Date(2018,01,01), end: new Date(2018,01,05), color: "#f0f0f0" }
+		]
+	}
+];
